63fd9bcb765e02372b6f0432ae23248d841e5aeb
Davidjimenez05 <davidjimenez05@iesamachado.org>
2024-03-13 23:01:54 +0100

    i

--- Arrays/validar.js
+++ Arrays/validar.js
@@ -1,33 +1,26 @@
 function validar(elementos){
     let estanCorrectos = true;
     for (var i=0;i<elementos.length;i++){
-        document.getElementById("campo"+(i+1).toString()).innerHTML = "";        
-
-        if (elementos[i].value == "" || (i==4 && !elementos[i].checked)){
-
+        document.getElementById("campo"+(i+1).toString()).innerHTML = ""; // Por defecto el campo está OK       
         if (elementos[i].value == "" || (i==6 && !elementos[i].checked)){
-
+            // Si el campo está vacío o (el campo es la casilla de verificación y no está marcada) entonces ...
             document.getElementById("campo"+(i+1).toString()).innerHTML = "El campo " + elementos[i].id + " está vacío";
             estanCorrectos = false;
         }
         
     }
-    if (!validarEmail()){
+    if (!validarEmail()){ // Si no es válido el correo
         document.getElementById("campo3").innerHTML = "Email no válido";        
         estanCorrectos = false;
     }
-
-    if (!validarEmail()){ //valido el correo
-        document.getElementById("campo3").innerHTML = "Email no válido";        
+    if (!validaPasswords()){ // Si no son válidas las contraseñas
+        document.getElementById("campo4").innerHTML = "La contraseña no cumple con requisitos de longitud o no coinciden";        
+        document.getElementById("campo5").innerHTML = "La contraseña no cumple con requisitos de longitud o no coinciden";
         estanCorrectos = false;
     }
-    if (!validaPasswords()){
-        document.getElementById("campo4").innerHTML = "La contraseña no cumple con los requisitos de longitud o no coincide"
-        document.getElementById("campo5").innerHTML = "La contraseña no cumple con los requisitos de longitud o no coincide"
-        estanCorrectos = false;
-    }   
     if (!validarDNI()){
-        document.getElementById("campo6").innerHTML = "DNI no valido (12345678X)"
+        document.getElementById("campo6").innerHTML = "DNI no válido (12345678X)";
+        estanCorrectos = false;
     }
     return estanCorrectos;
 }
@@ -45,37 +38,36 @@ function validarEmail(){
 	}
     return valido;
 } 
-
-}
 function validaPasswords(){
-    let clave1 = document.getElementById("password").value;
+    // return (document.getElementById("password1").value == document.getElementById("password2").value) && document.getElementById("password2").value.length>=8;
+    let clave1 = document.getElementById("password1").value;
     let clave2 = document.getElementById("password2").value;
-    let passwordsOK = true; //cumple con los requisitos establecidos
+    let passwordsOK = true; // Cumple con los requisitos establecidos
     if (clave1.length<8 || (clave1!=clave2))
-    passwordsOK = false;
-    return passwordsOK; 
+        passwordsOK = false;
+    return passwordsOK;
 }
+
+
+
+
 
 function validarDNI(){
-    let letra= ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
-    var cadena = document.getElementById("dni").value //DNI completo
-    var numero = parseInt(cadena.substring(0,8)); // parte numerica
-    var letraUsuario = cadena[8]; // Letra escrita por el usuario
-    var letraReal = letra[numero%23]; // Letra "Real" del DNI calculada segun "formula"
-    var dniValido =true; 
-    if (letraUsuario!=letraReal) //Si no coinciden las letras
+    var letra=['T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'];
+    var cadena = document.getElementById("dni").value; // DNI Completo
+    var numero = parseInt(cadena.substring(0,8)); // Parte numérica
+    var letraUsuario = cadena[8].toUpperCase(); // Letra escrita por el usuario, el string..toUpperCase() sirve para que convierta la letra a mayus el sistema
+    var letraReal = letra[numero%23]; // Letra "real" del DNI calculada según "fórmula"
+    var dniValido =true;
+    if (letraUsuario!=letraReal) // Si no coincide letras es falso
         dniValido = false;
     return dniValido;
 }
-function limpia(){
-    let cadena = document.getElementById("cadena").value;
-    let cadenaSinEspacios = cadena.replace(/ /g,"");
-    let cadenaLimpia = cadenaSinEspacios.toUpperCase();
-    document.getElementById("resultado").innerHTML = cadenaLimpia;
+
+function ceroespacios(){
+    dni.addEventListener("keyup",e=>{
+        let string = e.target.value
+        e.target.value = string.trim();
+    
+    })
 }
-
-// ¿Serías capaz de utilizar la etiqueta <form> ... </form> y que se vieran los mensajes sin 
-//enviar los datos al servidor cuando el formulario no estuviera correctamente relleno? 
-//Pista: revisa cómo podemos modificar el comportamiento predeterminado de un formulario 
-//(https://desarrolloweb.com/faq/explicaciones-detalladas-sobre-el-metodo-preventdefault-de-javascript).
-
